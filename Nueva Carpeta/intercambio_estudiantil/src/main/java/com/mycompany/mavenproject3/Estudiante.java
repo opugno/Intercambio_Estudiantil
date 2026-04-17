14c2b8a4fdf644f4e1b5277fa01f270cfc1af048
opugno <maura.gonzalez.s@mail.pucv.cl>
2026-04-17 15:31:45 -0400

    Modificaciones Tramite y Estudiante
    Modificacione y creacion de metodos en clase Tramite, asi como definicion de atributos y contructor de clase Estudiante

--- Nueva Carpeta/intercambio_estudiantil/src/main/java/com/mycompany/mavenproject3/Estudiante.java
+++ Nueva Carpeta/intercambio_estudiantil/src/main/java/com/mycompany/mavenproject3/Estudiante.java
@@ -5,6 +5,24 @@ import java.util.*;
  *
  * @author sonailslove
  */
-public class Estudiante {
-
+public class Estudiante 
+{
+    //ATRIBUTOS
+    private String rut;
+    private String nombre;
+    private String carrera;
+    private int anioIngreso;
+    private String estadoProceso; //"Postulación", "Aceptado", "Rechazado", "En curso"
+    private Convenio convenio;
+    
+    //CONSTRUCTOR
+    public Estudiante(String rut, String nombre, String carrera, int anioIngreso, String estadoProceso, Convenio convenio)
+    {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.carrera = carrera;
+        this.anioIngreso = anioIngreso;
+        this.estadoProceso = estadoProceso;
+        this.convenio = convenio;
+    }
 }
--- Nueva Carpeta/intercambio_estudiantil/src/main/java/com/mycompany/mavenproject3/Tramite.java
+++ Nueva Carpeta/intercambio_estudiantil/src/main/java/com/mycompany/mavenproject3/Tramite.java
@@ -30,4 +30,77 @@ public class Tramite.java
         this.documento = new HashMap<>();
         this.convenio = convenio;
     }
+
+    //SETTERS Y GETTERS
+    public String getIdTramite(){
+        return idTramite;
+    }
+    public void setIdTramite(String idTramite){
+        this.idTramite = idTramite;
+    }
+    
+    public Estudiante getEstudiante(){
+        return estudiante;
+    }
+    public void setEstudiante(Estudiante estudiante)
+    {
+        this.estudiante = estudiante;
+    }
+    
+   // public Map<TipoDocumento, DocumentoSubido> getDocumentos(){
+   //     return documento;
+   // }
+    public Map<TipoDocumento, DocumentoSubido> getDocumentos()
+    {
+        return Collections.unmodifiableMap(new HashMap<>(documento));
+    }
+    public void setDocumento(Map<TipoDocumento, DocumentoSubido> documento){
+        this.documento = documento;
+    }
+    
+    public Estado getEstado(){
+        return estado;
+    }
+    public void setEstado(Estado estado){
+        this.estado = estado;
+    }
+
+    //METODOS y sobrecarga de metodos
+    public void subirDocumento(TipoDocumento tipo, String nombreArchivo) 
+    {
+        documento.put(tipo, new DocumentoSubido(tipo, nombreArchivo, java.time.LocalDate.now()));
+    }
+    public void subirDocumento(String tipo, String nombreArchivo) 
+    {
+        subirDocumento(TipoDocumento.valueOf(tipo), nombreArchivo);
+    }
+
+    // Si se cumplen todos los requisitos del convenio
+    public boolean estaCompleto(Set<TipoDocumento> requisitos) 
+    {
+        for (TipoDocumento req : requisitos) 
+        {
+            if (!documento.containsKey(req)) 
+                return false;
+        }
+        return true;
+    }
+    
+    public void subirDocumentoSeguro(TipoDocumento tipo, String nombreArchivo) throws DocumentoDuplicadoException 
+    {
+    if (documentosYaTiene(tipo)) {
+        throw new DocumentoDuplicadoException("El documento " + tipo + " ya está subido para este trámite.");
+    }
+    subirDocumento(tipo, nombreArchivo);
+    }
+
+    private boolean documentosYaTiene(TipoDocumento tipo) 
+    {
+        return this.getDocumentos().containsKey(tipo);
+    }
+    
+    public boolean eliminarDocumento(TipoDocumento tipo) {
+        return documento.remove(tipo) != null;
+    }
+
 }
