90e9c92352113444bb47859d1d4bb5280ad8f193
opugno <maura.gonzalez.s@mail.pucv.cl>
2026-04-17 15:27:35 -0400

    Tramite
    Definición de atributos y contructor

--- Nueva Carpeta/intercambio_estudiantil/src/main/java/com/mycompany/mavenproject3/Tramite.java
+++ Nueva Carpeta/intercambio_estudiantil/src/main/java/com/mycompany/mavenproject3/Tramite.java
@@ -5,6 +5,29 @@ import java.util.*;
  *
  * @author sonailslove
  */
-public class Tramite.java {
-
+public class Tramite.java 
+{
+    public enum Estado {EN_PROCESO, COMPLETO}
+    
+    //ATRIBUTOS
+    private String idTramite;
+    private Estudiante estudiante;
+    private Map<TipoDocumento, DocumentoSubido> documento = new HashMap<>();
+    private Estado estado =  Estado.EN_PROCESO;
+    private Convenio convenio;
+    
+    //CONSTRUCTOR
+    public Tramite(String idTramite, Estudiante estudiante)
+    {
+        this.idTramite = idTramite;
+        this.estudiante = estudiante;
+    }
+    
+    public Tramite(String idTramite, Estudiante estudiante, Convenio convenio) {
+        this.idTramite = idTramite;
+        this.estudiante = estudiante;
+        this.estado = Estado.EN_PROCESO;
+        this.documento = new HashMap<>();
+        this.convenio = convenio;
+    }
 }
